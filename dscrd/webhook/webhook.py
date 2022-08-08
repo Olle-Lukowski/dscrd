5beac2e972df1df730ac01945b8fceffeea3cb14
Olle Lukowski <ollelukowski@hotmail.com>
2022-08-08 16:45:03 +0000

    Added some things

--- dscrd/webhook/webhook.py
+++ dscrd/webhook/webhook.py
@@ -4,13 +4,14 @@ class Webhook:
     def __init__(self, id: str):
         self.url = "https://discord.com/api/webhooks/" + id
 
-    def send_message(self, message: str, tts: bool = False, embeds: list = None, allowed_mentions: dict = None):
+    def send_message(self, message: str, tts: bool = False, embeds: list = None, allowed_mentions: dict = None, message_reference: dict = None):
         """Send a message to the webhook."""
         data = {
             "content": message,
             "tts": tts,
             "embeds": embeds,
             "allowed_mentions": allowed_mentions
+            "message_reference": message_reference
         }
         return requests.post(self.url, json=data)
     
